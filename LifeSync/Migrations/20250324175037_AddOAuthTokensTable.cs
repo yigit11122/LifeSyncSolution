4ce2580a785d04c872265790f59d689fa4c674d2
yigit gursoy <yigitgursoy2008@outlook.com>
2025-04-15 03:43:59 +0300

    yorum

--- LifeSync/Migrations/20250324175037_AddOAuthTokensTable.cs
+++ LifeSync/Migrations/20250324175037_AddOAuthTokensTable.cs
@@ -48,6 +48,7 @@ namespace LifeSync.Migrations
         }
 
         /// <inheritdoc />
+        ///denemeeeeee1222222222111111111212
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
