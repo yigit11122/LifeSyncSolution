e5791ef19b73c2f79f7590e3230311e3ef8ff762
yigit gursoy <yigitgursoy2008@outlook.com>
2025-04-28 03:43:30 +0300

    update_al_4

---
+++ LifeSync/Pages/AISuggestions.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LifeSync.Pages
+{
+    public class AISuggestionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
